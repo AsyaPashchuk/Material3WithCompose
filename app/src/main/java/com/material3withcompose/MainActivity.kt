b41a9022c2706108ce5497cd15651d6a2af66160
Viacheslav Panasiuk <badgerle82@gmail.com>
2022-12-14 12:33:22 +0200

    migrate ui theme from Material2 to Material3

--- app/src/main/java/com/material3withcompose/MainActivity.kt
+++ app/src/main/java/com/material3withcompose/MainActivity.kt
@@ -3,6 +3,10 @@ package com.material3withcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.material3withcompose.ui.theme.Material3WithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,6 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3WithComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
 
             }
         }
--- app/src/main/java/com/material3withcompose/ui/theme/Color.kt
+++ app/src/main/java/com/material3withcompose/ui/theme/Color.kt
@@ -2,7 +2,42 @@ package com.material3withcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green10 = Color(0xff003314)
+val Green20 = Color(0xff006627)
+val Green30 = Color(0xff00993b)
+val Green40 = Color(0xff00cc4e)
+val Green80 = Color(0xff99ffc0)
+val Green90 = Color(0xffccffe0)
+
+val DarkGreen10 = Color(0xff0d260d)
+val DarkGreen20 = Color(0xff194d19)
+val DarkGreen30 = Color(0xff267326)
+val DarkGreen40 = Color(0xff339933)
+val DarkGreen80 = Color(0xffb3e6b3)
+val DarkGreen90 = Color(0xffd9f2d9)
+
+val Violet10 = Color(0xff330033)
+val Violet20 = Color(0xff660066)
+val Violet30 = Color(0xff990099)
+val Violet40 = Color(0xffcc00cc)
+val Violet80 = Color(0xffff99ff)
+val Violet90 = Color(0xffffccff)
+
+val Red10 = Color(0xff410001)
+val Red20 = Color(0xff680003)
+val Red30 = Color(0xff930006)
+val Red40 = Color(0xffba1b1b)
+val Red80 = Color(0xffffb4a9)
+val Red90 = Color(0xffffdad4)
+
+val Grey10 = Color(0xff191c1d)
+val Grey20 = Color(0xff2d3132)
+val Grey90 = Color(0xffe0e3e3)
+val Grey95 = Color(0xffeff1f1)
+val Grey99 = Color(0xfffbfdfd)
+
+val GreenGrey30 = Color(0xff316847)
+val GreenGrey50 = Color(0xff52ad76)
+val GreenGrey60 = Color(0xff74be92)
+val GreenGrey80 = Color(0xffbadec8)
+val GreenGrey90 = Color(0xffdcefe4)
